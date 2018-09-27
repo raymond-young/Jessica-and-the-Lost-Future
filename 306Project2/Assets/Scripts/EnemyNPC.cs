9bbdf4917808ac7cdfa174afeb99c036bf7a4f8c
yianni1 <32092681+yianni1@users.noreply.github.com>
2018-09-27 22:46:07 +1200

    NPC movement work

---
+++ 306Project2/Assets/Scripts/EnemyNPC.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyNPC : MonoBehaviour {
+
+    private Rigidbody2D enemyBody;
+    public float xBound = 0;
+    public int maxXBound = 2;
+    
+   
+	// Use this for initialization
+	void Start () {
+        enemyBody = gameObject.GetComponent<Rigidbody2D>();
+        enemyBody.velocity = new Vector2(1, 0);
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        xBound = gameObject.transform.position.x;
+
+        //x-direction
+        if (xBound >= maxXBound)
+        {
+            enemyBody.velocity = new Vector2(-1, 0);
+        }
+        else if (xBound <= (maxXBound * - 1))
+        {
+            enemyBody.velocity = new Vector2(1, 0);
+        }
+
+    }
+}
--- 306Project2/Assets/Scripts/PlayerController.cs
+++ 306Project2/Assets/Scripts/PlayerController.cs
@@ -1,20 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
+    private Rigidbody2D playerBody;
+    private int speed = 10;
+    public Slider healthBar;
+
 	// Use this for initialization
 	void Start () {
-        
+        playerBody = gameObject.GetComponent<Rigidbody2D>();
     }
-	
-	// Update is called once per frame
-	void LateUpdate () {
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 20.0f;
-        var y = Input.GetAxis("Vertical") * Time.deltaTime * 20.0f;
 
-        transform.Translate(x, 0, 0);
-        transform.Translate(0, y, 0);
+    // Update is called once per frame
+    void FixedUpdate() {
+
+
+        var x = Input.GetAxis("Horizontal");
+        var y = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(x, y);
+
+        //Movement of character * speed
+        playerBody.velocity = (movement * speed);
+
+        //Prevents rotation on collision
+        if (playerBody.rotation != 0)
+        {
+            playerBody.rotation = 0;
+            onDamage(10.0);
+  
+        }
+
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Prevents rotation of character on corners
+        playerBody.angularVelocity = 0;
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        //Prevents rotation of character on corners
+        playerBody.angularVelocity = 0;
+    }
+
+    private void onDamage(double damagePercent)
+    {
+        Debug.Log(healthBar.value);
+        healthBar.value = healthBar.value - (float)(damagePercent / 100.0);
+        //RectTransform rt = healthBar.GetComponent<RectTransform>();
+
+        // double change = rt.rect.width * (damagePercent / 100.0);
+
+        // rt.sizeDelta = new Vector2(rt.rect.width - (float) change, rt.rect.height);
+        //  rt.position = new Vector2(rt.position.x - (float)change, rt.position.y);
+
     }
 }
