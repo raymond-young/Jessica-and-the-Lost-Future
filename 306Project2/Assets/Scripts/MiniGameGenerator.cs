ead6443303500d99e89509351f1dfba1533bca8b
Edwar Zhang <Edwar.zhang1@gmail.com>
2018-09-30 16:21:10 +1300

    worked out canvas to screen sizes

---
+++ 306Project2/Assets/Scripts/MiniGameGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MiniGameGenerator : MonoBehaviour {
+	public GameObject left;
+	public GameObject right;
+	public GameObject up;
+
+	public GameObject down;
+	void Start () {
+		GameObject test = Instantiate(up);
+		RectTransform parentRectTransform = gameObject.GetComponent<RectTransform>();
+		float canvasWidth = gameObject.GetComponentInParent<Canvas>().pixelRect.width;
+		float canvasHeight = gameObject.GetComponentInParent<Canvas>().pixelRect.height;
+		test.GetComponent<RectTransform>().SetParent(parentRectTransform);
+		test.GetComponent<RectTransform>().localPosition = new Vector2(canvasWidth,canvasHeight);
+	}
+}
