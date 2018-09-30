e2663b7a35d7ea34c2803a5c25cf2a2faa8fb196
Edwar Zhang <Edwar.zhang1@gmail.com>
2018-09-30 22:22:11 +1300

    reset the minigame scene

--- 306Project2/Assets/Scripts/Minigame.cs
+++ 306Project2/Assets/Scripts/Minigame.cs
@@ -1,87 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Minigame : MonoBehaviour {
     ///////////////////////////////////
 
     enum Hardness { Easy, Hard};
 
+    enum Key { Up, Down, Left, Right};
     //////////////////////////////////
 
-    enum Key { Up, Down, Left, Right};
-    int keyNum = 4;
-    int timeLimit;
+    /* Game Stats */
+    //int keyNum ;
+    float timeLimit = 10f;
     int currentIndex;
+    float currentTime;
+    //Config
+    private float HolderYOffset = -1.5f;
+    private float ArrowSize = 10f;
+    private float ArrowGap = 100f;
 
-    public GameObject HolderPrefab;
+    /* Prefabs */
     public GameObject UpKeyPrefab;
     public GameObject DownKeyPrefab;
     public GameObject LeftKeyPrefab;
     public GameObject RightKeyPrefab;
+    public Slider BarPrefab;
 
+    /* Game Objects */
     List<GameObject> gameKeys = new List<GameObject>();
- 
-
-    //private Keys[] keys;
-    private Key[] keys = {Key.Up, Key.Down, Key.Left, Key.Right};
-
- /* Don't worry about anything between this, still working on it */
-    //Trying to generate the minigame bar according to the screen size and number of arrows
-
-    GameObject holder;
+    private Slider bar;
     
-    //Coefficient
-    //Number of arrows changes according to hardness
-    private int maxArrowNum = 10;
-
-    private float holderSizeVsScreenWidth = 0.8f;
-    private float holderYOffsetVsScreenHeight = 0.1f;
-
-    private float holderX;
-    private float holderY;
-    private float holderWidth;
-    private float holderHeight;
-    private float arrowSize;
-
-    private void CalcMeasurements()
-    {
-        int screenWidth = Screen.width;
-        int screenHeight = Screen.height;
-        holderX = 0f;
-        holderY = screenHeight * holderYOffsetVsScreenHeight;
-
-        arrowSize = (screenWidth * holderSizeVsScreenWidth) / maxArrowNum;
-
-        holderHeight = arrowSize;
-        holderWidth = arrowSize * keyNum;
-  
-
-        
-        holder = Instantiate(HolderPrefab);
-        holder.transform.position = holder.transform.position + new Vector3(0, -2, 0);
-        Vector3 pos = new Vector3(holderX, holderY, 0);
-
-        Debug.Log("scale = " + pos);
-        //holder = Instantiate(HolderPrefab, holder.transform.position + pos, transform.rotation);
-        holder.GetComponent<SpriteRenderer>().color = Color.red;
-
-    }
- /* Don't worry about anything between this */
-
+    /* Default settings for testing */
+    private Key[] keys = {Key.Up, Key.Down, Key.Left, Key.Right};
+    int keyNum = 4;
+    
     // Use this for initialization
     void Start () {
         //Initialise keyNum and timeLimit according to hardness
         //keys = getKeys();
-
         Debug.Log("Started\n");
-        //Generate holder
-        //holder = Instantiate(HolderPrefab, transform.position, transform.rotation);
-        //holder.transform.localScale = new Vector2(1, 1);
-        CalcMeasurements();
 
 
         //Generate game keys
+        int count = 0;
         foreach (Key k in keys)
         {
             GameObject key;
@@ -104,24 +67,32 @@ public class Minigame : MonoBehaviour {
                     break;
             }
             gameKeys.Add(key);
-            key.transform.parent = holder.transform;
+            //key.transform.parent = bar.transform;
+            key.transform.SetParent(gameObject.GetComponentInParent<Canvas>().transform);
+            key.transform.position = transform.position;
+            //key.transform.localScale = new Vector3(ArrowSize, ArrowSize, 0);
+            count++;
         }
         currentIndex = 0;
+
+        //Set up time bar
+        bar = Instantiate(BarPrefab);
+        bar.transform.SetParent(gameObject.GetComponentInParent<Canvas>().transform);
+        bar.transform.position = transform.position;
+        currentTime = 0;
 	}
 
+    
     bool correct = false;
     bool pressed = false;
-    
     // Update is called once per frame
     void Update ()
     {
-        Debug.Log("Current index = " + currentIndex + "\n");
         correct = false;
         pressed = false;
         switch (keys[currentIndex])
         {
             case Key.Up:
-                Debug.Log("Current index = " + currentIndex + "\n");
                 Debug.Log("Need to press Up \n");
                 if (Input.GetKeyDown(KeyCode.UpArrow))
                 {
@@ -136,7 +107,6 @@ public class Minigame : MonoBehaviour {
                 }
                 break;
             case Key.Down:
-                Debug.Log("Current index = " + currentIndex + "\n");
                 Debug.Log("Need to press Down \n");
                 if (Input.GetKeyDown(KeyCode.DownArrow))
                 {
@@ -150,7 +120,6 @@ public class Minigame : MonoBehaviour {
                 }
                 break;
             case Key.Left:
-                Debug.Log("Current index = " + currentIndex + "\n");
                 Debug.Log("Need to press Left \n");
                 if (Input.GetKeyDown(KeyCode.LeftArrow))
                 {
@@ -165,7 +134,6 @@ public class Minigame : MonoBehaviour {
                 }
                 break;
             case Key.Right:
-                Debug.Log("Current index = " + currentIndex + "\n");
                 Debug.Log("Need to press Right \n");
                 if (Input.GetKeyDown(KeyCode.RightArrow))
                 {
@@ -180,20 +148,17 @@ public class Minigame : MonoBehaviour {
                 }
                 break;
          }
-         Debug.Log("Corrent = " + correct + "\n");
-         Debug.Log("Pressed = " + pressed + "\n");
+
         if (pressed)
         {
             if (correct)
             {
                 //Change color of the correctly pressed key
-                Debug.Log("Pressed right key. Change color!");
                 gameKeys[currentIndex].GetComponent<SpriteRenderer>().color = Color.green;
                 //Still more keys to press
                 if (currentIndex < gameKeys.Count) 
                 {
                     currentIndex++;
-                    Debug.Log("More key to press. Index changed to " + currentIndex);
                 }
                 //Finish game
                 else 
@@ -210,6 +175,13 @@ public class Minigame : MonoBehaviour {
                 currentIndex = 0;
                 Debug.Log("Pressed wrong key. Start over. Index = " + currentIndex);
             }
+        }
+        //Update bar
+        bar.value = Mathf.Lerp(0f, 1f, currentTime / timeLimit);
+        currentTime += Time.deltaTime;
+        if (currentTime >= timeLimit)
+        {
+            Fail();
         }
 	}
 
@@ -231,4 +203,63 @@ public class Minigame : MonoBehaviour {
         Debug.Log("Game Finished. You won");
     }
 
+    public void Fail()
+    {
+        Debug.Log("Game Finished. You lost");
+    }
 }
+
+
+
+
+
+
+
+/* Don't worry about anything between this, still working on it 
+   //Trying to generate the minigame bar according to the screen size and number of arrows
+
+
+   //Coefficient
+   //Number of arrows changes according to hardness
+   private int maxArrowNum = 10;
+
+   private float holderSizeVsScreenWidth = 0.8f;
+   private float holderYOffsetVsScreenHeight = 0.1f;
+
+   private float holderX;
+   private float holderY;
+   private float holderWidth;
+   private float holderHeight;
+   private float arrowSize;
+
+   private void CalcMeasurements()
+   {
+       int screenWidth = Screen.width;
+       int screenHeight = Screen.height;
+       holderX = 0f;
+       holderY = screenHeight * holderYOffsetVsScreenHeight;
+
+       arrowSize = (screenWidth * holderSizeVsScreenWidth) / maxArrowNum;
+
+       holderHeight = arrowSize;
+       holderWidth = arrowSize * keyNum;
+
+
+
+       holder = Instantiate(HolderPrefab);
+       holder.transform.position = holder.transform.position + new Vector3(0, -2, 0);
+       Vector3 pos = new Vector3(holderX, holderY, 0);
+
+       Debug.Log("scale = " + pos);
+       //holder = Instantiate(HolderPrefab, holder.transform.position + pos, transform.rotation);
+       holder.GetComponent<SpriteRenderer>().color = Color.red;
+
+   }
+
+
+    
+        //Generate holder
+        holder = Instantiate(HolderPrefab);
+        holder.transform.position = new Vector3(0, HolderYOffset, 0);
+        //holder.transform.localScale = new Vector3(ArrowSize * keyNum, ArrowSize, 0);
+Don't worry about anything between this */
