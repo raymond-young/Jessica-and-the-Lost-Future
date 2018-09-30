4cbe4951170d60cd4f7b8f63b61f1de169307c6e
yianni1 <32092681+yianni1@users.noreply.github.com>
2018-09-30 16:54:37 +1300

    Added three segments

--- 306Project2/Assets/Scripts/SegmentFactory/ThreeSegments.cs
+++ 306Project2/Assets/Scripts/SegmentFactory/ThreeSegments.cs
@@ -4,8 +4,32 @@ using UnityEngine;
 
 public class ThreeSegments : ISegment
 {
-    public List<Vector2> CalculatePoints(Vector2 orignalPos, Vector2 endPos, bool clockwise)
+    public List<Vector2> CalculatePoints(Vector2 origonalPos, Vector2 thirdPos, bool clockwise)
     {
-        throw new System.NotImplementedException();
+        List<Vector2> rectanglePoints = new List<Vector2>();
+
+        Vector2 secondPos;
+        Vector2 fourthPos;
+        if (clockwise)
+        {
+            secondPos = new Vector2(origonalPos.x, thirdPos.y);
+            fourthPos = new Vector2(thirdPos.x, origonalPos.y);
+
+        }
+        else
+        {
+            secondPos = new Vector2(thirdPos.x, origonalPos.y);
+            fourthPos = new Vector2(origonalPos.x, thirdPos.y);
+        }
+
+        //Important ordering of adding to rectangle
+        rectanglePoints.Add(secondPos);
+        rectanglePoints.Add(thirdPos);
+        rectanglePoints.Add(fourthPos);
+        rectanglePoints.Add(thirdPos);
+        rectanglePoints.Add(secondPos);
+        rectanglePoints.Add(origonalPos);
+
+        return rectanglePoints;
     }
 }
