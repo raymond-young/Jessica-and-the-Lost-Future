c522ebf27973e7f6d7945ca041b715de269fa364
Ray <raymondyoung122@gmail.com>
2018-09-30 16:58:07 +1300

    Added TwoSegment

--- 306Project2/Assets/Scripts/SegmentFactory/TwoSegments.cs
+++ 306Project2/Assets/Scripts/SegmentFactory/TwoSegments.cs
@@ -4,8 +4,30 @@ using UnityEngine;
 
 public class TwoSegments : ISegment
 {
-    public List<Vector2> CalculatePoints(Vector2 orignalPos, Vector2 endPos, bool clockwise)
+     public List<Vector2> CalculatePoints(Vector2 origonalPos, Vector2 endPos, bool clockwise)
     {
-        throw new System.NotImplementedException();
+        List<Vector2> rectanglePoints = new List<Vector2>();
+
+        Vector2 secondPos;
+        Vector2 fourthPos;
+        if (clockwise)
+        {
+            secondPos = new Vector2(origonalPos.x, endPos.y);
+            fourthPos = new Vector2(endPos.x, origonalPos.y);
+
+        }
+        else
+        {
+            secondPos = new Vector2(endPos.x, origonalPos.y);
+            fourthPos = new Vector2(origonalPos.x, endPos.y);
+        }
+
+        //Important ordering of adding to rectangle
+        rectanglePoints.Add(secondPos);
+        rectanglePoints.Add(endPos);
+        rectanglePoints.Add(secondPos);
+        rectanglePoints.Add(origonalPos);
+
+        return rectanglePoints;
     }
 }
