4a59cf4ab05e3d477a694a6331fcc24e087f8fbe
Jenny Lee <jlee923@aucklanduni.ac.nz>
2018-10-01 18:09:15 +1300

    added font and finished exit scene

---
+++ 306Project2/Assets/Scripts/CongratulationsUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CongratulationsUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MainMenuButtonClicked()
+    {
+        SceneManager.LoadScene("WelcomeScene");
+    }
+
+    public void QuitButtonClicked()
+    {
+        Application.Quit();
+    }
+}
