904e7e36cd854dcd82c93eac91caeb062aca054d
Calcifer <tsmpanda@gmail.com>
2018-10-03 15:49:08 +1300

    Stair can move.

--- 306Project2/Assets/Scripts/Hop.cs
+++ 306Project2/Assets/Scripts/Hop.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hop : MonoBehaviour {
+public class Hop : MonoBehaviour
+{
+    enum Arrow { LeftArrow, RightArrow };
+
     public GameObject stairPrefab;
     public GameObject blobPrefab;
 
     private Queue<GameObject> stairQueue = new Queue<GameObject>();
     private GameObject lastStair;
+    private Queue<Arrow> stairRef = new Queue<Arrow>();
     private int numOfStairs;
 
     static System.Random random = new System.Random();
@@ -23,21 +27,27 @@ public class Hop : MonoBehaviour {
 
     private float x;
     private float y;
+    private float speed;
 
     // Use this for initialization
     void Start () {
+        //Initialise configurations
         numOfStairs = 10;
         timeLimit = 10f;
         
         x = stairPrefab.GetComponent<RectTransform>().rect.width;
-        y = gameObject.GetComponentInParent<Canvas>().pixelRect.height /2;
+        y = gameObject.GetComponentInParent<Canvas>().pixelRect.height / 2;
+        speed = stairPrefab.GetComponent<RectTransform>().rect.height / 10;
         lastStair = null;
-        //Debug.Log(lastStair);
+
         generateStair();
         generateStair();
+
+        currentTime = 0;
     }
 
 
+    //One call of this function will generate one new stair and add it to the queue
     void generateStair()
     {
         GameObject stair = Instantiate(stairPrefab);
@@ -46,36 +56,85 @@ public class Hop : MonoBehaviour {
         RectTransform stairRectTransform = stair.GetComponent<RectTransform>();
         stairRectTransform.SetParent(parentRectTransform);
 
+        //Decide the Y postion of the new stair according to the previous one's y position
         float thisY;
         if (lastStair != null)
         {
-            Debug.Log("previous Y " + lastStair.GetComponent<RectTransform>().localPosition.y);
-            thisY = 80 + lastStair.GetComponent<RectTransform>().localPosition.y;
+            thisY = blobPrefab.GetComponent<RectTransform>().rect.height * 1.5f +
+                lastStair.GetComponent<RectTransform>().rect.height +
+                lastStair.GetComponent<RectTransform>().localPosition.y;
         } 
         else
         {
             thisY = y;
         }
 
-        Debug.Log("thisY: " + thisY);
-
         if (random.Next(0, 2) == 0)
         {
-            stairRectTransform.localPosition = new Vector2(x, thisY);   
+            stairRectTransform.localPosition = new Vector2(x, thisY);
+            stairRef.Enqueue(Arrow.RightArrow);
         }
         else
         {
             stairRectTransform.localPosition = new Vector2(-x, thisY);
+            stairRef.Enqueue(Arrow.LeftArrow);
         }
         stairQueue.Enqueue(stair);
         lastStair = stair;
     }
 
 
+    void OnGUI()
+    {
+        //Listen to key press
+        if (currentTime > timeLimit)
+        {
+            Finish();
+        }
 
-	
-	// Update is called once per frame
-	void Update () {
-		
+        //Listen to key press event
+        Event e = Event.current;
+        if (e.type == EventType.KeyDown)
+        {
+            //If the right key was pressed, change color of the game object and move to the next one
+            if (e.keyCode.ToString().Equals(stairRef.Peek().ToString()) && e.keyCode != KeyCode.None)
+            {
+                stairQueue.Dequeue();
+            }
+            else
+            {
+                Fail();
+            }
+        }
+    }
+
+    
+    void Update () {
+		//Update 
+        foreach (GameObject stair in stairQueue)
+        {
+            RectTransform stairRectTransform = stair.GetComponent<RectTransform>();
+            float newY = stairRectTransform.localPosition.y - speed;
+            float newX = stairRectTransform.localPosition.x;
+            stairRectTransform.localPosition = new Vector2(newX, newY);
+
+        }
+        currentTime += Time.deltaTime;
 	}
+
+
+
+    private void Finish()
+    {
+        Debug.Log("finished");
+        //Notify the game manager that the player has successfully finished the game
+        //GameObject.FindGameObjectWithTag("MiniGameManager").GetComponent<MiniGameManager>().FinishGame(true);
+    }
+
+    private void Fail()
+    {
+        Debug.Log("failed");
+        //Notify the game manager that the player has failed the game
+        //GameObject.FindGameObjectWithTag("MiniGameManager").GetComponent<MiniGameManager>().FinishGame(false);
+    }
 }
