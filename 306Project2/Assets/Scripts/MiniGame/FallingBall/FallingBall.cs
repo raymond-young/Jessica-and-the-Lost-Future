d99b78a1b0ce795188d2ae65b921a6cfe2ec1fbd
Calcifer <tsmpanda@gmail.com>
2018-10-09 14:31:41 +1300

    attemp to move backet in fallingball

--- 306Project2/Assets/Scripts/MiniGame/FallingBall/FallingBall.cs
+++ 306Project2/Assets/Scripts/MiniGame/FallingBall/FallingBall.cs
@@ -1,24 +1,68 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FallingBall : MonoBehaviour {
     public GameObject ballPrefab;
-
+    public GameObject backet;
+    public Text count;
+    
 
     List<GameObject> balls = new List<GameObject>();
 
     int numOfBalls = 10;
+    int goal;
     float xRange;
     float y;
+
+    float leftWall;
+    float rightWall;
+
 	// Use this for initialization
 	void Start () {
+        goal = 98;
+        count.text = goal.ToString();
+        
+
         xRange = gameObject.GetComponentInParent<Canvas>().pixelRect.width / 2;
         y = gameObject.GetComponentInParent<Canvas>().pixelRect.height / 2;
+
+        rightWall = gameObject.GetComponentInParent<Canvas>().pixelRect.width / 2;
+        leftWall = -rightWall;
     }
-	
-	// Update is called once per frame
-	void Update () {
+    
+
+
+    void OnGUI()
+    {
+        //Finish game when tehre is no more arrows to press
+        if (goal == 0)
+        {
+            Finish();
+        }
+        //if (gameStart)
+        //{
+            //Listen to key press event
+            Event e = Event.current;
+            if (e.type == EventType.KeyDown)
+            {
+                float x = backet.GetComponent<RectTransform>().localPosition.x;
+                if (e.keyCode.ToString().Equals("LeftArrow") && x > leftWall)
+                {
+                    backet.GetComponent<RectTransform>().localPosition.x = x - 10;
+                }
+                else if (e.keyCode.ToString().Equals("RightArrow") && x < rightWall)
+                {
+                    backet.GetComponent<RectTransform>().localPosition.x = x + 10;
+
+                }
+            }
+       // }
+    }
+
+    // Update is called once per frame
+    void Update () {
         
         if (balls.Count < numOfBalls)
         {
@@ -28,4 +72,18 @@ public class FallingBall : MonoBehaviour {
             balls.Add(ball);
         }
 	}
+
+
+    private void Finish()
+    {
+        //Notify the game manager that the player has successfully finished the game
+        //GameObject.FindGameObjectWithTag("MiniGameManager").GetComponent<MiniGameManager>().FinishGame(true);
+        Debug.Log("finished");
+    }
+
+    private void Fail()
+    {
+        //Notify the game manager that the player has failed the game
+        //GameObject.FindGameObjectWithTag("MiniGameManager").GetComponent<MiniGameManager>().FinishGame(false);
+    }
 }
