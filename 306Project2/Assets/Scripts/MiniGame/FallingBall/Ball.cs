90e35148a772a352a28dbf54483cc8c76cda8e94
Calcifer <tsmpanda@gmail.com>
2018-10-09 13:59:24 +1300

    Implemented Falling ball generation

---
+++ 306Project2/Assets/Scripts/MiniGame/FallingBall/Ball.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+        float f = Random.Range(9000, 10000);
+        gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.down * f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ 306Project2/Assets/Scripts/MiniGame/FallingBall/FallingBall.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingBall : MonoBehaviour {
+    public GameObject ballPrefab;
+
+
+    List<GameObject> balls = new List<GameObject>();
+
+    int numOfBalls = 10;
+    float xRange;
+    float y;
+	// Use this for initialization
+	void Start () {
+        xRange = gameObject.GetComponentInParent<Canvas>().pixelRect.width / 2;
+        y = gameObject.GetComponentInParent<Canvas>().pixelRect.height / 2;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (balls.Count < numOfBalls)
+        {
+            GameObject ball = Instantiate(ballPrefab);
+            ball.GetComponent<RectTransform>().SetParent(gameObject.GetComponent<RectTransform>());
+            ball.GetComponent<RectTransform>().localPosition = new Vector2(Random.Range(0, xRange) * Random.Range(-1f, 1f), y);
+            balls.Add(ball);
+        }
+	}
+}
