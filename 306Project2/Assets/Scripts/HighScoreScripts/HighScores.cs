78c07f8bc61706cf861c193e2cba9ac43907a9da
Yianni Bares <ybar417@aucklanduni.ac.nz>
2018-10-15 13:47:08 +1300

    High score screen UI changes

--- 306Project2/Assets/Scripts/HighScoreScripts/HighScores.cs
+++ 306Project2/Assets/Scripts/HighScoreScripts/HighScores.cs
@@ -11,9 +11,20 @@ public class HighScores : MonoBehaviour {
     public GameObject level2Panel;
     public GameObject level3Panel;
 
+    public GameObject easyButton;
+    public GameObject hardButton;
+
     public GameObject playerListPrefab;
 
+    private Color color = new Color(0.9433962f, 0.511748f, 0.511748f);
+    private Color levelColor = new Color(0.1135636f, 0.5811083f, 0.8301887f);
+
     private float heightInterval = 30;
+
+    private GameObject highScorePanel;
+
+    //Initialize gamemode to 0 for easy (default) at 1 for hard
+    private int gameMode = 0;
 
     private List<GameObject> currentPlayers = new List<GameObject>();
 
@@ -27,7 +38,12 @@ public class HighScores : MonoBehaviour {
 
         //Loads players to appropriate level
         ReadFile(0);
-       
+
+        //Sets up buttons, default easy is selected
+        easyButton.GetComponent<Image>().color = color;
+
+        //Gets reference to highscorepanel
+        highScorePanel = GameObject.FindGameObjectWithTag("HighScorePanel");
     }
 
     // Update is called once per frame
@@ -57,8 +73,9 @@ public class HighScores : MonoBehaviour {
         if (level == 0)
         {
             GameObject p = Instantiate(playerListPrefab, parentPanel.transform);
-            p.transform.localPosition = new Vector2(0f, 105f);
+            p.transform.localPosition = new Vector2(0f, 160f);
             p.GetComponent<Text>().text = "yianni 50";
+            p.GetComponent<Text>().fontSize = 40;
             p.name = "yianni";
 
             //Read file code here
@@ -82,38 +99,85 @@ public class HighScores : MonoBehaviour {
     public void Level1()
     {
         RefreshPlayers("yianni", 200);
+
+        //Toggle panels
         level1Panel.SetActive(true);
         level2Panel.SetActive(false);
         level3Panel.SetActive(false);
         parentPanel.SetActive(false);
+
+        //Color change code
+        for (int i = 0; i < highScorePanel.transform.childCount; i++)
+        {
+            highScorePanel.transform.GetChild(i).GetComponent<Image>().color = Color.white;
+        }
+        highScorePanel.transform.GetChild(0).GetComponent<Image>().color = levelColor;
+
     }
 
     public void Level2()
     {
+        //Toggle panels
         level1Panel.SetActive(false);
         level2Panel.SetActive(true);
         level3Panel.SetActive(false);
         parentPanel.SetActive(false);
+
+        //Color change code
+        for (int i = 0; i < highScorePanel.transform.childCount; i++)
+        {
+            highScorePanel.transform.GetChild(i).GetComponent<Image>().color = Color.white;
+        }
+        highScorePanel.transform.GetChild(1).GetComponent<Image>().color = levelColor;
     }
 
     public void Level3()
     {
+        //Toggle panels
         level1Panel.SetActive(false);
         level2Panel.SetActive(false);
         level3Panel.SetActive(true);
         parentPanel.SetActive(false);
+
+
+        //Color change code
+        for (int i = 0; i < highScorePanel.transform.childCount; i++)
+        {
+            highScorePanel.transform.GetChild(i).GetComponent<Image>().color = Color.white;
+        }
+        highScorePanel.transform.GetChild(2).GetComponent<Image>().color = levelColor;
     }
 
     public void Total()
     {
+        //Toggle panels
         level1Panel.SetActive(false);
         level2Panel.SetActive(false);
         level3Panel.SetActive(false);
         parentPanel.SetActive(true);
+
+        //Color change code
+        for (int i = 0; i < highScorePanel.transform.childCount; i++)
+        {
+            highScorePanel.transform.GetChild(i).GetComponent<Image>().color = Color.white;
+        }
+        highScorePanel.transform.GetChild(3).GetComponent<Image>().color = levelColor;
     }
 
     public void BackToMenu()
     {
         SceneManager.LoadScene("WelcomeScene");
+    }
+
+    public void OnEasyBtn()
+    {
+        easyButton.GetComponent<Image>().color = color;
+        hardButton.GetComponent<Image>().color = Color.white;
+    }
+
+    public void OnHardBtn()
+    {
+        hardButton.GetComponent<Image>().color = color;
+        easyButton.GetComponent<Image>().color = Color.white;
     }
 }
