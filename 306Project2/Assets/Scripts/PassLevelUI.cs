0f8424cba7f82d190c7ea17720b39f0d0d001d2e
Edwar Zhang <Edwar.zhang1@gmail.com>
2018-10-18 02:14:54 +1300

    fixed flow of scenes

---
+++ 306Project2/Assets/Scripts/PassLevelUI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PassLevelUI : MonoBehaviour {
+
+    private string score;
+    public UnityEngine.UI.Text scoreText;
+	// Use this for initialization
+	void Start () {
+        GameObject object1 = GameObject.FindGameObjectWithTag("scoreTransferObject");
+        score = object1.GetComponent<ScoreTransferScript>().getScore();
+        scoreText.text = score.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void continueButtonClicked()
+    {
+        SceneManager.LoadScene("Level-1");
+    }
+
+    public void mainMenuButtonClicked()
+    {
+        SceneManager.LoadScene("WelcomeScene");
+    }
+
+    public void setScore(float score)
+    {
+        scoreText.text = score.ToString();
+    }
+}
