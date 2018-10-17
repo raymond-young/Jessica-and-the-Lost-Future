4cd95838e14d5fe65cc6c762853102724be0164c
Jenny Lee <jlee923@aucklanduni.ac.nz>
2018-10-17 21:18:13 +1300

    complete level 3 with dialogue and confidence increase

--- 306Project2/Assets/Scripts/GoodNPC.cs
+++ 306Project2/Assets/Scripts/GoodNPC.cs
@@ -87,4 +87,13 @@ public class GoodNPC : Movement {
         
     }
 
+    [YarnCommand("moveSophia")]
+    public void MoveSophia(string destination)
+    {
+        Debug.Log("move sophia called");
+        Vector3 move = new Vector3(gameObject.transform.localPosition.x, -0f, 0);
+        gameObject.transform.localPosition = move;
+
+    }
+
 }
